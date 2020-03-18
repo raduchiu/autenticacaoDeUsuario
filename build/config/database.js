8ef8d5a7f2fdab000d577d2931a49407d5a3e0a9
andraderaul <theandraderaul@gmail.com>
2020-03-17 22:59:07 -0300

    build config

--- build/config/database.js
+++ build/config/database.js
@@ -1,12 +1,26 @@
-"use strict";module.exports = {
-  dialect: 'postgres',
-  host: 'localhost',
-  username: 'postgres',
-  password: 'docker',
-  database: 'goBarber',
-  define: {
-    timerstamps: true,
-    underscored: true,
-    underscoredAll: true,
-  },
-};
+"use strict";
+process.env.NODE_ENV === 'development' ?
+  module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: 'docker',
+    database: 'goBarber',
+    define: {
+      timerstamps: true,
+      underscored: true,
+      underscoredAll: true,
+    },
+  } :
+  module.exports = {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+      ssl: true
+    },
+    define: {
+      timerstamps: true,
+      underscored: true,
+      underscoredAll: true,
+    }
+  };
